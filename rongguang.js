a13b29a67bb0459bc51bed2135d6a8f99c25c244
llb0824-bb <480316883@qq.com>
2024-09-14 00:12:31 +0800

    Create rongguang.js

---
+++ rongguang.js
@@ -0,0 +1,18 @@
+var body = $response.body;
+//我们就没有进行请求链接匹配了
+var obj = JSON.parse(body);
+//修改
+ obj = JSON.parse(body);
+ obj = JSON.parse($response.body);
+obj.data.ITEM_ID = "yubi10006";
+body = JSON.stringify(obj);
+$done({body});
+
+/*
+参数说明
+title: 主标题
+subtitle: 副标题
+body: 消息正文
+*/
+
+$notification.post("Storm Sniffer","Network Debug Tool","充值修改成功!!!")

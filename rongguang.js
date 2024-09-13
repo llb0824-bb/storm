3f675e0d3857293997d1848055cc186d4238a2e8
llb0824-bb <480316883@qq.com>
2024-09-14 00:26:44 +0800

    Update rongguang.js

--- rongguang.js
+++ rongguang.js
@@ -7,7 +7,6 @@ var obj = JSON.parse(body);
 obj.data.ITEM_ID = "yubi10006";
 body = JSON.stringify(obj);
 $done({body});
-//测试成功
 /*
 参数说明
 title: 主标题
@@ -15,6 +14,6 @@ subtitle: 副标题
 body: 消息正文
 */
 
-$notification.post("Storm Sniffer","Network Debug Tool","充值修改成功!!!")
+$notification.post("Storm Sniffer","Network Debug Tool","最后的荣光充值修改成功!!!")
 
 
